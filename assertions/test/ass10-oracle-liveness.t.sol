ff525135787b4c144f1c9d05f9aec633459b5d83
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2025-04-10 19:03:00 +0200

    fix import

--- assertions/test/ass10-oracle-liveness.t.sol
+++ assertions/test/ass10-oracle-liveness.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {Test} from "credible-std/lib/forge-std/src/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {Credible} from "credible-std/Credible.sol";
 import {OracleLiveness} from "../src/ass10-oracle-liveness.sol";
 import {IOracle} from "../src/ass10-oracle-liveness.sol";
