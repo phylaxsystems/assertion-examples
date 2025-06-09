9e4023f6855fcdb51f8bc1a82d343a570d651190
Jacob Stenum Czepluch <j.czepluch@proton.me>
2025-06-09 18:10:12 +0200

    rename to ass3

--- assertions/src/ass3-pending-balance-bedrock-staking.a.sol
+++ assertions/src/ass3-pending-balance-bedrock-staking.a.sol
@@ -2,53 +2,37 @@
 pragma solidity ^0.8.13;
 
 import {Assertion} from "credible-std/Assertion.sol";
-import {Staking} from "src/assX-protocol-pending-balance-on-mint.sol";
+import {Staking} from "src/ass3-pending-balance-bedrock-staking.sol";
 import {PhEvm} from "credible-std/PhEvm.sol";
 import {Test} from "forge-std/Test.sol";
 
 contract StakingAssertions is Assertion, Staking, Test {
     function triggers() public view override {
-        triggerRecorder.registerCallTrigger(
-            this.assertTotalPendingOnMint.selector,
-            Staking.mint.selector
-        );
+        triggerRecorder.registerCallTrigger(this.assertTotalPendingOnMint.selector, Staking.mint.selector);
     }
 
     // Assertion PostTotalPending = PreTotalPending + ethersToMint
     function assertTotalPendingOnMint() public {
         Staking staking = Staking(payable(ph.getAssertionAdopter()));
 
-        PhEvm.CallInputs[] memory mintCalls = ph.getCallInputs(
-            address(staking),
-            Staking.mint.selector
-        );
+        PhEvm.CallInputs[] memory mintCalls = ph.getCallInputs(address(staking), Staking.mint.selector);
 
         ph.forkPreState();
         uint256 preTotalPending = staking.getPendingEthers();
 
         uint256 expectedPostTotalPending;
         for (uint256 i = 0; i < mintCalls.length; i++) {
-            (uint256 minToMint, ) = abi.decode(
-                mintCalls[i].input,
-                (uint256, uint256)
-            );
+            (uint256 minToMint,) = abi.decode(mintCalls[i].input, (uint256, uint256));
             expectedPostTotalPending += minToMint;
-            require(
-                ctx.postTotalPending == ctx.preTotalPending + ctx.ethersToMint,
-                "TotalPendingInvalid"
-            );
+            require(ctx.postTotalPending == ctx.preTotalPending + ctx.ethersToMint, "TotalPendingInvalid");
         }
 
         ph.forkPostState();
 
-        require(
-            staking.getPendingEthers() == expectedPostTotalPending,
-            "ExpectedTotalPendingInvalid"
-        );
+        require(staking.getPendingEthers() == expectedPostTotalPending, "ExpectedTotalPendingInvalid");
     }
 
     function _load(bytes32 slot) internal view returns (bytes32 value) {
         value = ph.load(ph.getAssertionAdopter(), slot);
     }
-
 }
