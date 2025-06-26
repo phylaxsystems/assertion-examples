4a9268410f8b1b2b2e745318246fc438edd0f347
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2025-06-26 16:26:43 +0200

    fix imports for ass2 (#19)

--- src/ass2-erc4626-operations.sol
+++ src/ass2-erc4626-operations.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC4626.sol";
-import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/token/ERC20/extensions/ERC4626.sol";
+import "openzeppelin-contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-contracts/access/Ownable.sol";
 
 /**
  * @title CoolVault
